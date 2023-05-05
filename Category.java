5f8f387668f8bd122d0cb21157d9ffe775d01a30
hazem magdy <101430887+hazemmagdy20@users.noreply.github.com>
2023-05-05 15:45:23 +0300

    Update Category.java

--- Category.java
+++ Category.java
@@ -16,11 +16,23 @@ public class Category {
         this.name = name;
     }
 
-    public void add_item(Item item) {
-        // add item to category
+    public void add_item(Item item) { //add item
+        for(int i = 0 ; i < this.items.length ; i++)
+        {
+            if(this.items[i] == null) //if current index is empty.
+            {
+                this.items[i] = item ;
+                break;
+            }
+        }
     }
 
     public void display_items() {
-        // display items in category
+        System.out.println("Items that is in Category: " + this.name);
+        for (Item item : items) { //for each item in items_array.
+            if (item != null) { //if current index is not empty .
+            System.out.println(item);
+            }
     }
+}
 }
