ab5408d42fe75611866da860e4b4f8be12b0cfd0
hazem magdy <101430887+hazemmagdy20@users.noreply.github.com>
2023-05-05 15:48:50 +0300

    Update Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -6,14 +6,37 @@ public class Catalog {
     }
 
     public void display_categories() {
-        // display categories code
+        System.out.println("Categories in the Catalog:");
+        for (Category category : categories) {
+            System.out.println(category.get_name());
+        }
     }
 
     public void display_items_in_category(String category_name) {
-        // display items in category code
+        System.out.println("Items in Category: " + category_name);
+        for (Category category : categories) {
+            if (category_name == category.get_name()) {
+                category.display_items();
+            }
+        }
+        System.out.println("Category not found.");
     }
 
     public void search_for_item(String name) {
-        // search for item code
+        System.out.println("Searching for item with name is: " + name);
+        boolean is_found = false;
+        for (Category category : categories) {
+            for (Item item : category.get_items()) {
+                if (item != null && name == item.get_name()) { 
+                    System.out.println("Item is found.");
+                    System.out.println(item);
+                    is_found = true;
+                    
+                }
+            }
+        }
+        if (!is_found) {
+            System.out.println("Item not found.");
+        }
     }
 }
