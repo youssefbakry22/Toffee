cc81df04cfb9e3697fc3c55193e9d6a0276ba763
ahmed walid <90784364+abowalid808@users.noreply.github.com>
2023-05-12 21:09:20 +0300

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,24 +1,42 @@
+import java.util.ArrayList;
+
 public class ShoppingCart {
-    private Item[] items;
+       private ArrayList<Item> items;
 
-    public ShoppingCart() {
-        this.items = new Item[100];
+        public ShoppingCart() {
+           items=new ArrayList<Item>(100);
+
+        }
+
+        public ArrayList<Item> get_items() {
+            // implementation of ShoppingCart.get_items
+            return items;
+        }
+
+        public void add_item(Item item, double quantity) {
+            // implementation of ShoppingCart.add_item
+            items.add(item);
+            item.set_quantity(quantity);
+        }
+
+        public void display_items() {
+            // implementation of ShoppingCart.display_items
+            for(int i=0;i<items.size();i++){
+                System.out.print("# "+(i+1));
+                System.out.print(items[i]);
+                System.out.println("quantity: "+item.get_quantity());
+            }
+        }
+
+        public boolean is_empty() {
+            // implementation of ShoppingCart.is_empty
+            if(items.size()==0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
     }
 
-    public Item[] get_items() {
-        // implementation of ShoppingCart.get_items
-    }
-
-    public void add_item(Item item, double quantity) {
-        // implementation of ShoppingCart.add_item
-    }
-
-    public void display_items() {
-        // implementation of ShoppingCart.display_items
-    }
-
-    public boolean is_empty() {
-        // implementation of ShoppingCart.is_empty
-    }
-
-}
