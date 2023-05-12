7148474d78f02dc87b1ec8fb688afb4a6491187e
ahmed walid <90784364+abowalid808@users.noreply.github.com>
2023-05-12 21:19:35 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 public class Order {
     private int id;
     private String status;
     private double total_price;
-    private Item[] items;
+    private ArrayList<Item>  items;
 
-    public Order(int id, String status, Item[] items) {
+    public Order(int id, String status, ArrayList<Item> items) {
         this.id = id;
         this.status = status;
         this.items = items;
@@ -13,25 +15,34 @@ public class Order {
 
     public int get_id() {
         // implementation of Order.get_id
+        return id;
     }
 
     public String get_status() {
         // implementation of Order.get_status
+        return status;
     }
 
     public double get_total_price() {
         // implementation of Order.get_total_price
+        return total_price;
     }
 
     public void set_id(int id) {
         // implementation of Order.set_id
+        this.id=id;
     }
 
     public void set_status(String status) {
         // implementation of Order.set_status
+        this.status=status;
     }
 
     public double calc_total_price() {
         // implementation of Order.calc_total_price
+        for(int i=0;i<items.size();i++){
+            total_price+=items[i].get_price()*items[i].get_quantity();
+        }
+        return total_price;
     }
 }
